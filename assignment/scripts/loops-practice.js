3b1e43a834c6d517236ed9aea22be9bb2f7079f1
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-03-04 18:11:58 -0600

    3.2 finished

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -62,6 +62,18 @@ while(i<starsLength){
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
+let max = 6;
+i = 0;
+while(i < max){
+  console.log(i);
+  i++;
+}
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+
+i = 10;
+let min = 4;
+while (i > min){
+  console.log(i);
+  i--;
+}
